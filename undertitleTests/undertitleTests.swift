f7ac4f8396ac64c9726a257f15dace5ec4bf9f0e
Carlos L <clagccs@gmail.com>
2026-06-15 22:26:56 -0400

    Initial Commit

---
+++ undertitleTests/undertitleTests.swift
@@ -0,0 +1,16 @@
+//
+//  undertitleTests.swift
+//  undertitleTests
+//
+//  Created by Carlos Laguna Medina on 15/6/26.
+//
+
+import Testing
+
+struct undertitleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
